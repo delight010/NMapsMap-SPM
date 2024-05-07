8379abb172482aeb29a3ba8dc58702adc85753bf
SungAh Oh <osa070@knou.ac.kr>
2024-05-07 13:34:25 +0900

    feat: NMapsMap Package file add

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NMapsMap",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "NMapsMap",
+            targets: ["NMapsMap"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "NMapsMap",
+            path: "https://github.com/delight010/NMapsMap-SPM/releases/download/v3.18.1/NMapsMap.xcframework.zip"
+        )
+    ]
+)
